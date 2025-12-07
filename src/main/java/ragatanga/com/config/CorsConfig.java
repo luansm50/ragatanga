d9906251002dab2f708a6704b2396f9bd2e448c6
Luan Melo <luan.melo@effecti.com.br>
2025-12-07 10:16:23 -0300

    commit

---
+++ src/main/java/ragatanga/com/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package ragatanga.com.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // ou coloque a URL do seu front-end
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+
+}
